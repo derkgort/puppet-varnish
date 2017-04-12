77a3b154654b34e3d8fc0501951b8e9e533a4129
Derk Gortemaker <derk@gortemaker.nl>
2017-04-12 16:21:58 +0200

    centos/redhat repo switch when version > 5

--- manifests/repo/el6.pp
+++ manifests/repo/el6.pp
@@ -1,9 +1,35 @@
 # Add the Varnish repo
 class varnish::repo::el6 {
-  yumrepo { 'varnish-cache':
-    baseurl  => "https://repo.varnish-cache.org/redhat/varnish-${::varnish::varnish_version}/el6/",
-    descr    => 'Varnish-cache RPM repository',
-    enabled  => 1,
-    gpgcheck => 0
+
+  $majorversionparts = split($::varnish::varnish_version, '[.]')
+  $majorversion = $majorversionparts[0]
+
+  case $majorversion {
+    '5': {
+      $repo = 'packagecloud'
+    }
+    default: {
+      $repo = 'legacy'
+    }
+  }
+
+  case $repo {
+    'packagecloud': {
+      yumrepo { 'varnish-cache':
+        baseurl  => "https://packagecloud.io/varnishcache/varnish${majorversion}/el/${::operatingsystemmajrelease}/\$basearch",
+        descr    => 'Varnish-cache RPM repository',
+        enabled  => 1,
+        gpgcheck => 0,
+        priority => 10,
+      }
+    }
+    'legacy': {
+      yumrepo { 'varnish-cache':
+        baseurl  => "https://repo.varnish-cache.org/redhat/varnish-${::varnish::varnish_version}/el6/",
+        descr    => 'Varnish-cache RPM repository',
+        enabled  => 1,
+        gpgcheck => 0
+      }
+    }
   }
 }
